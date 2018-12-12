8b7f0599c0cf620006950ea92ebe0e06ef1e2a80
Matt Gibbs <mgibbs@slac.stanford.edu>
2018-12-12 11:24:32 -0800

    Fix some import issues.

--- edmbutton/edm_button_qtplugin.py
+++ edmbutton/edm_button_qtplugin.py
@@ -1,6 +1,5 @@
 from pydm.widgets.qtplugin_base import qtplugin_factory, WidgetCategory
-from pydm.widgets.baseplot_qtplugin import qtplugin_plot_factory
-from .edm_button import PyDMEDMDisplayButton
+from edmbutton import PyDMEDMDisplayButton
 # EDM Display Button plugin
 print("Loading PyDMEDMDisplayButtonPlugin!")
 PyDMEDMDisplayButtonPlugin = qtplugin_factory(PyDMEDMDisplayButton, group=WidgetCategory.DISPLAY, is_container=True)
