2802bc0290b00ced2546b43484438b1a5fe92564
Matt Gibbs <mgibbs@slac.stanford.edu>
2019-12-16 12:47:06 -0800

    Bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='edmbutton',
-    version='0.2.0',
+    version='0.2.1',
     description='A PyDM widget for launching EDM displays.',
     long_description=long_description,
 
